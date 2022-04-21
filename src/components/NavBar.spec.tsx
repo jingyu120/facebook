da30759e783d82248ac7d97793578a87ca3cf4a9
Justin <jingyu120@gmail.com>
2022-04-21 17:24:56 -0500

    add middle nav bar icons and add layout test

--- src/components/NavBar.spec.tsx
+++ src/components/NavBar.spec.tsx
@@ -3,8 +3,10 @@ import NavBar from "./NavBar";
 
 describe("Navigation bar", () => {
   describe("Layout", () => {
-    it("has fb logo", () => {
+    beforeEach(() => {
       render(<NavBar />);
+    });
+    it("has fb logo", () => {
       const fbIcon = screen.getByRole("img");
       expect(fbIcon).toHaveAttribute(
         "src",
@@ -13,6 +15,29 @@ describe("Navigation bar", () => {
       expect(fbIcon).toHaveAttribute("alt", "fb logo");
     });
 
-    it("has home icon", () => {});
+    it("has home icon", () => {
+      const homeIcon = screen.getByRole("button", { name: "home-icon" });
+      expect(homeIcon).toBeInTheDocument();
+    });
+
+    it("has watch icon", () => {
+      const watchIcon = screen.getByRole("button", { name: "watch-icon" });
+      expect(watchIcon).toBeInTheDocument();
+    });
+
+    it("has market icon", () => {
+      const marketIcon = screen.getByRole("button", { name: "market-icon" });
+      expect(marketIcon).toBeInTheDocument();
+    });
+
+    it("has group icon", () => {
+      const groupIcon = screen.getByRole("button", { name: "group-icon" });
+      expect(groupIcon).toBeInTheDocument();
+    });
+
+    it("has game icon", () => {
+      const gameIcon = screen.getByRole("button", { name: "game-icon" });
+      expect(gameIcon).toBeInTheDocument();
+    });
   });
 });
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+import {
+  HomeIcon,
+  DesktopComputerIcon,
+  ShoppingBagIcon,
+  UserGroupIcon,
+  PuzzleIcon,
+} from "@heroicons/react/solid";
 
 type Props = {};
 
@@ -8,12 +15,30 @@ function NavBar({}: Props) {
       <div>
         {/*left*/}
         <img
+          aria-aria-label=""
           className="w-10"
           alt="fb logo"
           src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/2048px-Facebook_f_logo_%282021%29.svg.png"
         />
       </div>
-      <div>{/*middle*/}</div>
+      <div>
+        {/*middle*/}
+        <button className="w-10" aria-label="home-icon">
+          <HomeIcon />
+        </button>
+        <button className="w-10" aria-label="watch-icon">
+          <DesktopComputerIcon />
+        </button>
+        <button className="w-10" aria-label="market-icon">
+          <ShoppingBagIcon />
+        </button>
+        <button className="w-10" aria-label="group-icon">
+          <UserGroupIcon />
+        </button>
+        <button className="w-10" aria-label="game-icon">
+          <PuzzleIcon />
+        </button>
+      </div>
       <div>{/*right*/}</div>
     </div>
   );
