5ef5f049e89f503a66c41dc0bbbd48dc55a76514
Justin <jingyu120@gmail.com>
2022-04-21 12:52:19 -0500

    implement login page tests

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -3,7 +3,26 @@ import React from "react";
 type Props = {};
 
 function LoginPage({}: Props) {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <div>
+        <h1>facebook</h1>
+        <h2>Connect with friends and the world around you on Facebook.</h2>
+      </div>
+      <div>
+        <div>
+          <input placeholder="Email" type="email" />
+          <input placeholder="Password" type="password" />
+        </div>
+        <div>
+          <button>Log In</button>
+          <a href="google.com">Forgot password?</a>
+        </div>
+        <hr />
+        <button>Create new account</button>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
