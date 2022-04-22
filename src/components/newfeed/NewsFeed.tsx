2bf2cbb9a0b4403cc74525b00a7d4469ba2780be
Justin <jingyu120@gmail.com>
2022-04-22 18:35:28 -0500

    add mock data for story

--- src/components/newfeed/NewsFeed.tsx
+++ src/components/newfeed/NewsFeed.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import Story from "./Story";
+import mockData from "../../MOCK_DATA.json";
 
 type Props = {};
 
 function NewsFeed({}: Props) {
-  return <div>NewsFeed</div>;
+  const [data, setData] =
+    React.useState<
+      {
+        id: number;
+        first_name: string;
+        last_name: string;
+        image: string;
+        profile: string;
+      }[]
+    >();
+  React.useEffect(() => {
+    setData(mockData);
+  }, []);
+  console.log(data);
+
+  return (
+    <div className="flex flex-col">
+      <div>
+        <Story />
+      </div>
+    </div>
+  );
 }
 
 export default NewsFeed;
