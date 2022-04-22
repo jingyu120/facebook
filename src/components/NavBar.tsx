f786c46c804566b7003d2f36e9884fc664b106df
Justin <jingyu120@gmail.com>
2022-04-22 15:06:29 -0500

    add css for nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,23 +16,23 @@ type Props = {};
 
 function NavBar({}: Props) {
   return (
-    <div>
-      <div>
+    <div className="bg-[#18191a] flex justify-between items-center h-[56px]">
+      <div className="flex items-center">
         {/*left*/}
         <img
           aria-label="fb-logo"
-          className="w-10"
+          className="w-10 mx-4"
           alt="fb logo"
           src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/2048px-Facebook_f_logo_%282021%29.svg.png"
         />
-        <div className="flex border rounded-lg w-[200px]">
+        <div className="flex border rounded-lg w-[250px] bg-white px-3">
           <span title="search">
             <SearchIcon className="w-6" />
           </span>
-          <input placeholder="Search Facebook" />
+          <input className="px-2" placeholder="Search Facebook" />
         </div>
       </div>
-      <div>
+      <div className="space-x-8 text-white">
         {/*middle*/}
         <button className="w-10" aria-label="home-icon">
           <HomeIcon />
@@ -50,11 +50,11 @@ function NavBar({}: Props) {
           <PuzzleIcon />
         </button>
       </div>
-      <div>
+      <div className="flex text-white space-x-4">
         {/*right*/}
-        <button>
+        <button className="flex items-center bg-blue-300 px-3 py-1 rounded-3xl text-blue-500">
           <img
-            className="w-10 rounded-full"
+            className="w-10 rounded-full mr-3"
             src="https://scontent-ort2-2.xx.fbcdn.net/v/t1.6435-9/92690763_10158917196618274_8488492201817407488_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=PDik7fYKiFEAX-cXsYp&_nc_ht=scontent-ort2-2.xx&oh=00_AT8nZ2l0v2cDtby3e3sq-oygsbk2Ocr1JREsW3E3YKEzLw&oe=62879F37"
             alt="profile-pic"
           />
